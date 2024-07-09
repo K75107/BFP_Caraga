465848050310338ad6ce7bd59d870fc54941c528
divineGrace101 <divinagracia.kent123@gmail.com>
2024-07-10 00:05:49 +0800

    change the hover color

--- frontend/src/routes/sidebar.js
+++ frontend/src/routes/sidebar.js
@@ -27,61 +27,61 @@ const Sidebar = () => {
         </div>
         <nav className="px-4 py-3 flex-grow">
           <ul className="space-y-1">
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/dashboard') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/dashboard') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/dashboard" className="flex items-center space-x-2">
                 <RxDashboard className='mr-3'/>
                 <span>Dashboard</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/users') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/users') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/users" className="flex items-center space-x-2">
                 <HiOutlineUsers className='mr-3'/>
                 <span>Users</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/incomeStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/incomeStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/incomeStatement" className="flex items-center space-x-2">
                 <SlNotebook className='mr-3' />
                 <span>Income Statement</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/balanceSheet') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/balanceSheet') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/balanceSheet" className="flex items-center space-x-2">
                 <VscNotebookTemplate className='mr-3'/>
                 <span>Balance Sheet</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/cashflowStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/cashflowStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/cashflowStatement" className="flex items-center space-x-2">
                 <LiaMoneyBillWaveAltSolid className='mr-3'/>
                 <span>Cashflow Statement</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/changesInEquity') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/changesInEquity') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/changesInEquity" className="flex items-center space-x-2">
                 <AiOutlineBarChart className='mr-3'/>
                 <span>Changes In Equity</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/generalLedger') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/generalLedger') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/generalLedger" className="flex items-center space-x-2">
                 <VscNotebook className='mr-3'/>
                 <span>General Ledger</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/trialBalance') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/trialBalance') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/trialBalance" className="flex items-center space-x-2">
                 <CiViewTable className='mr-3'/>
                 <span>Trial Balance</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/deposits') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/deposits') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/deposits" className="flex items-center space-x-2">
                 <PiHandDeposit className='mr-3'/>
                 <span>Deposits</span>
               </Link>
             </li>
-            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/collections') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/collections') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
               <Link to="/main/collections" className="flex items-center space-x-2">
                 <BiCollection className='mr-3'/>
                 <span>Collections</span>
