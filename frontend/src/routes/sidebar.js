c0a5b6d32861107fb458f11d1625af005b20991c
divineGrace101 <divinagracia.kent123@gmail.com>
2024-07-10 00:04:10 +0800

    added active for the routers and hover

--- frontend/src/routes/sidebar.js
+++ frontend/src/routes/sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaTachometerAlt, FaUsers, FaFileInvoiceDollar, FaBalanceScale, FaMoneyBillWave, FaChartLine, FaBook, FaTable, FaPiggyBank, FaCashRegister } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 import { LuUserCircle } from "react-icons/lu";
 import { TbSettings2 } from "react-icons/tb";
 import { RxDashboard } from "react-icons/rx";
@@ -16,6 +15,9 @@ import { BiCollection } from "react-icons/bi";
 import logo from '../assets/logo.png'; // Update the path to your logo image
 
 const Sidebar = () => {
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
+
   return (
     <div className='bg-color-lighter-gray min-h-screen flex'>
       <div className="w-72 h-screen bg-color-white text-black shadow-xl font-body flex flex-col">
@@ -25,61 +27,61 @@ const Sidebar = () => {
         </div>
         <nav className="px-4 py-3 flex-grow">
           <ul className="space-y-1">
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold  ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/dashboard') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/dashboard" className="flex items-center space-x-2">
-                <RxDashboard  className='mr-3'/>
+                <RxDashboard className='mr-3'/>
                 <span>Dashboard</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/users') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/users" className="flex items-center space-x-2">
                 <HiOutlineUsers className='mr-3'/>
                 <span>Users</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/incomeStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/incomeStatement" className="flex items-center space-x-2">
                 <SlNotebook className='mr-3' />
                 <span>Income Statement</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/balanceSheet') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/balanceSheet" className="flex items-center space-x-2">
                 <VscNotebookTemplate className='mr-3'/>
                 <span>Balance Sheet</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/cashflowStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/cashflowStatement" className="flex items-center space-x-2">
                 <LiaMoneyBillWaveAltSolid className='mr-3'/>
                 <span>Cashflow Statement</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/changesInEquity') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/changesInEquity" className="flex items-center space-x-2">
                 <AiOutlineBarChart className='mr-3'/>
                 <span>Changes In Equity</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/generalLedger') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/generalLedger" className="flex items-center space-x-2">
                 <VscNotebook className='mr-3'/>
                 <span>General Ledger</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/trialBalance') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/trialBalance" className="flex items-center space-x-2">
                 <CiViewTable className='mr-3'/>
                 <span>Trial Balance</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/deposits') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/deposits" className="flex items-center space-x-2">
                 <PiHandDeposit className='mr-3'/>
                 <span>Deposits</span>
               </Link>
             </li>
-            <li className="hover:bg-color-dark-red hover:text-white px-3 py-3 rounded-md font-semibold ">
+            <li className={`px-3 py-3 rounded-md font-semibold ${isActive('/main/collections') ? 'bg-color-dark-red text-white' : 'hover:bg-color-dark-red hover:text-white'}`}>
               <Link to="/main/collections" className="flex items-center space-x-2">
                 <BiCollection className='mr-3'/>
                 <span>Collections</span>
@@ -92,9 +94,9 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="flex-1 pl-1 bg-gray-100">
-        <header className="flex items-center justify-between bg-white p-4 shadow-sm  mb-6">
-         <div></div>
-         <div className="flex items-center space-x-4">
+        <header className="flex items-center justify-between bg-white p-4 shadow-sm mb-6">
+          <div></div>
+          <div className="flex items-center space-x-4">
             <TbSettings2 className="text-2xl text-gray-700 hover:text-gray-900 cursor-pointer"/>
             <LuUserCircle className="text-2xl text-gray-700 hover:text-gray-900 cursor-pointer"/>
           </div>
