02496fe7da6174f8ddd86c611ffbc73c0d43d1c0
divineGrace101 <divinagracia.kent123@gmail.com>
2024-07-12 23:08:24 +0800

    added the LoginUnfinished to the main

--- frontend/src/routes/Authentication/login.js
+++ frontend/src/routes/Authentication/login.js
@@ -1,59 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock, faEye, faEyeSlash, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Login() {
- 
     const navigate = useNavigate();
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const handlePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     const handleSubmit = (event) => {
-      event.preventDefault(); // Prevent the default form submission
-      
-      // Navigate to the /Dashboard route
-      navigate('/Main/Dashboard');
+        event.preventDefault(); 
+        
+        navigate('/Main/Dashboard');
     };
- 
- 
+
     return (
-      <section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/> 
-          Flowbite    
-      </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6"  onSubmit={handleSubmit}>
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                  </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
-              </form>
-          </div>
-      </div>
-  </div>
-</section>
+        <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/bfpbackground.png)' }}>
+            <div className="w-96 p-5 h-96 bg-white shadow-lg">
+                <h1 className="text-3xl font-bold text-center mb-10">WELCOME</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="relative mb-4">
+                        <label htmlFor="username" className="block mb-2 font-medium"></label>
+                        <input type="text" id="username" placeholder="Username" name="username" className="w-full p-2.5 pl-10 border rounded-md border-gray-300" required />
+                        <FontAwesomeIcon icon={faEnvelope} className="absolute left-3 top-3.5 text-gray-400" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="password" className="block mb-2 font-medium"></label>
+                        <input type={passwordVisible ? "text" : "password"} id="password" placeholder="Password" name="password" className="w-full p-2.5 pl-10 pr-10 border rounded-md border-gray-300" required />
+                        <FontAwesomeIcon icon={faLock} className="absolute left-3 bottom-3.5 text-gray-400" />
+                        <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} onClick={handlePasswordVisibility} className="absolute right-3 bottom-3 text-gray-400 cursor-pointer" />
+                    </div>
+                    <div className="flex justify-between items-center mb-5">
+                        <div>
+                            <input type="checkbox" id="remember" name="remember" className="mr-1.5" />
+                            <label htmlFor="remember">Remember me</label>
+                        </div>
+                        <a href="#" className="text-blue-500">Forgot password?</a>
+                    </div>
+                    <button type="submit" className="w-full py-2.5 mt-12 bg-blue-500 text-white rounded-full">Log In</button>
+                </form>
+            </div>
+            <div className="ml-0 w-72 h-96 bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: 'url(/bfpbackground.png)' }}>
+                <img src="/bfplogo.png" alt="Logo" className="w-full h-full object-contain" />
+            </div>
+        </div>
     );
-  }
+}
