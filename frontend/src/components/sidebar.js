6e42c6999b6b7760a7f0efc05baa4e9579356fd4
divineGrace101 <divinagracia.kent123@gmail.com>
2024-07-24 02:29:55 +0800

    added toggle for the sidebar and initiate the react grid for users

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,97 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { RxDashboard } from "react-icons/rx";
 import { HiOutlineUsers } from "react-icons/hi2";
 import { SlNotebook } from "react-icons/sl";
-import { VscNotebook } from "react-icons/vsc";
-import { VscNotebookTemplate } from "react-icons/vsc";
+import { VscNotebook, VscNotebookTemplate } from "react-icons/vsc";
 import { LiaMoneyBillWaveAltSolid } from "react-icons/lia";
 import { AiOutlineBarChart } from "react-icons/ai";
 import { CiViewTable } from "react-icons/ci";
 import { PiHandDeposit } from "react-icons/pi";
 import { BiCollection } from "react-icons/bi";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import logo from '../assets/logo.png'; // Update the path to your logo image
 
 const Sidebar = () => {
   const location = useLocation();
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const isActive = (path) => location.pathname === path;
 
-  return (
-    
-      <div className="w-64 h-screen bg-color-white text-black font-body flex flex-col text-sm border-s ">
-        <div className="flex items-center p-2 border-s w-">
-          <img src={logo} alt="BFP Logo" className="h-10 w-10 mr-2 ml-5"/>
-          <h1 className='font-bold text-2xl'>BFP CARAGA</h1>
-          
-        </div>
-        
-        <nav className="px-4 py-3 flex-grow">
-          <ul className="space-y-1">
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/dashboard') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/dashboard" className="flex items-center space-x-2">
-                <RxDashboard className='mr-3'/>
-                <span>Dashboard</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/users') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/users" className="flex items-center space-x-2">
-                <HiOutlineUsers className='mr-3'/>
-                <span>Users</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/incomeStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/incomeStatement" className="flex items-center space-x-2">
-                <SlNotebook className='mr-3' />
-                <span>Income Statement</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/balanceSheet') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/balanceSheet" className="flex items-center space-x-2">
-                <VscNotebookTemplate className='mr-3'/>
-                <span>Balance Sheet</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/cashflowStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/cashflowStatement" className="flex items-center space-x-2">
-                <LiaMoneyBillWaveAltSolid className='mr-3'/>
-                <span>Cashflow Statement</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/changesInEquity') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/changesInEquity" className="flex items-center space-x-2">
-                <AiOutlineBarChart className='mr-3'/>
-                <span>Changes In Equity</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/generalLedger') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/generalLedger" className="flex items-center space-x-2">
-                <VscNotebook className='mr-3'/>
-                <span>General Ledger</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/trialBalance') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/trialBalance" className="flex items-center space-x-2">
-                <CiViewTable className='mr-3'/>
-                <span>Trial Balance</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/deposits') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/deposits" className="flex items-center space-x-2">
-                <PiHandDeposit className='mr-3'/>
-                <span>Deposits</span>
-              </Link>
-            </li>
-            <li className={`px-3 py-2 rounded-sm font-semibold ${isActive('/main/collections') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
-              <Link to="/main/collections" className="flex items-center space-x-2">
-                <BiCollection className='mr-3'/>
-                <span>Collections</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
+  return (
+    <div className={`flex h-screen ${isCollapsed ? 'w-16' : 'w-64'} bg-white text-black font-body flex-col text-sm border-r transition-all duration-300`}>
+      <div className="flex items-center p-2 border-b whitespace-nowrap">
+        <img 
+          src={logo} 
+          alt="BFP Logo" 
+          className={`h-8 w-8 mr-2 ml-2 ${isCollapsed ? 'cursor-pointer' : ''}`} 
+          onClick={isCollapsed ? toggleSidebar : null}
+        />
+        {!isCollapsed && (
+          <>
+            <h1 className='font-bold text-xl flex-1'>BFP CARAGA</h1>
+            <button onClick={toggleSidebar} className="ml-2">
+              <AiOutlineClose size={22} />
+            </button>
+          </>
+        )}
+      </div>
+      <nav className="px-2 py-3 flex-grow">
+        <ul className="space-y-1">
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/dashboard') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <RxDashboard size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Dashboard</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/users') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <HiOutlineUsers size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Users</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/incomeStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <SlNotebook size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Income Statement</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/balanceSheet') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <VscNotebookTemplate size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Balance Sheet</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/cashflowStatement') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <LiaMoneyBillWaveAltSolid size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Cashflow Statement</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/changesInEquity') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <AiOutlineBarChart size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Changes In Equity</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/generalLedger') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <VscNotebook size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>General Ledger</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/trialBalance') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <CiViewTable size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Trial Balance</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/deposits') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <PiHandDeposit size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Deposits</span>
+            </div>
+          </li>
+          <li className={`whitespace-nowrap flex items-center pl-3 py-2 rounded-sm font-semibold ${isActive('/main/collections') ? 'bg-color-dark-red text-white' : 'hover:bg-color-light-red'}`}>
+            <div className="flex items-center">
+              <BiCollection size={22} className="mr-3" />
+            </div>
+            <div className={`${isCollapsed ? 'hidden' : 'block'}`}>
+              <span>Collections</span>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
--- frontend/src/routes/users.js
+++ frontend/src/routes/users.js
@@ -7,9 +7,59 @@ import "@silevis/reactgrid/styles.css";
 
 
 export default function Users(){
+
+    const columns = [
+        { columnId: "Fire Station", width: 200 },
+        { columnId: "User Type", width: 200 },
+        { columnId: "Username", width: 200 },
+        { columnId: "Password", width: 200 },
+
+      ];
+      
+      const rows = [
+        {
+          rowId: 0,
+          cells: [
+            { type: "header", text: "Fire Station" },
+            { type: "header", text: "User Type" },
+            { type: "header", text: "Username" },
+            { type: "header", text: "Password" },
+          ],
+        },
+        {
+          rowId: 1,
+          cells: [
+            { type: "text", text: "Thomas" },
+            { type: "text", text: "Goldman" },
+          ],
+        },
+        {
+          rowId: 2,
+          cells: [
+            { type: "text", text: "Susie" },
+            { type: "text", text: "Spencer" },
+          ],
+        },
+        {
+          rowId: 3,
+          cells: [
+            { type: "text", text: "" },
+            { type: "text", text: "" },
+          ],
+        }
+      ];
+
+
     return(
         <div className="flex h-full">
-            <div className="flex-1 w-60 h-full bg-white rounded-md">
+            
+            <div className="flex-1 w-60 h-full bg-white rounded-3xl">
+                <div className="flex h-full mt-2">
+                    <ReactGrid
+                        rows={rows}
+                        columns={columns}
+                    />
+                </div>
 
             </div>
             
