c577e848795ce91ad3e0c3b2b93493451ef33135
Joshua Felicitas <felicitas.joshua251@gmail.com>
2024-09-19 18:01:59 +0800

    new commit

--- frontend/src/routes/Accounting/FireStations/fireStationOfficers.js
+++ frontend/src/routes/Accounting/FireStations/fireStationOfficers.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Modal from "../../../components/Modal";
 import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../config/firebase-config"; // 
 
 export default function FireStations() {
   const [showModal, setShowModal] = useState(false);
@@ -30,7 +31,8 @@ export default function FireStations() {
 
   const handleAddStation = async () => {
     try {
-        
+      // Add your logic for adding fire station data to Firebase here
+      // You might need to handle image upload if applicable
       console.log("Fire Station added:", { stationName, location, officerName, image });
     } catch (err) {
       console.error("Error adding fire station: ", err);
@@ -46,17 +48,17 @@ export default function FireStations() {
     <Fragment>
       <div className="bg-white h-full py-6 px-8 w-full rounded-lg">
         <div className="flex justify-between w-full">
-          <h1 className="text-[25px] font-semibold text-[#1E1E1E]">Manage Fire Stations</h1>
+          <h1 className="text-[25px] font-semibold text-[#1E1E1E]">Manage Officers</h1>
           <button 
             className="bg-[#2196F3] rounded-lg text-white py-2 px-3 text-[11px] font-medium" 
             onClick={() => setShowModal(true)}>
-              ADD FIRE STATION
+              ADD AN OFFICER
           </button>
         </div>
 
         <hr className="border-t border-[#7694D4] my-4" />
 
-        {/* Table to show list of fire stations */}
+        {/* Table to show list  */}
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
@@ -87,7 +89,7 @@ export default function FireStations() {
       <Modal isVisible={showModal}>
         <div className="bg-white w-[600px] h-auto rounded py-4 px-6">
           <div className="flex justify-between items-center">
-            <h1 className="font-bold text-[27px] text-[#1E1E1E]">Add New Fire Station</h1>
+            <h1 className="font-bold text-[27px] text-[#1E1E1E]">Add an Officer</h1>
             <button className="text-[27px] bg-transparent" onClick={() => setShowModal(false)}>×</button>
           </div>
 
