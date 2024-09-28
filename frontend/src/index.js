c1f4266216d84ef24196b734e0832f24ea7def11
SajiSensei <mrboogart@gmail.com>
2024-09-28 17:54:28 +0800

    fixed trialbalance name in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ import Accounts from './routes/Accounting/Admin/GeneralLedger/accounts';
 import LedgerDetails from './routes/Accounting/Admin/GeneralLedger/ledgerDetails';
 import LedgerList from './routes/Accounting/Admin/GeneralLedger/legderList';
 
-import TrialBalance from './routes/Accounting/Admin/TrialBalance/trialBalance';
+import TrialBalance from './routes/Accounting/Admin/TrialBalance/TrialBalance';
 import TrialBalanceList from './routes/Accounting/Admin/TrialBalance/TrialBalanceList';
 import TrialBalanceDetails from './routes/Accounting/Admin/TrialBalance/TrialBalanceDetails';
 
