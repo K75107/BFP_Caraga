12a5e1f7e3b213614c88827711ade284229f077a
divineGrace101 <divinagracia.kent123@gmail.com>
2024-09-28 20:16:02 +0800

    change table design

--- frontend/src/routes/Accounting/Admin/GeneralLedger/generalLedger.js
+++ frontend/src/routes/Accounting/Admin/GeneralLedger/generalLedger.js
@@ -6,7 +6,7 @@ export default function GeneralLedger() {
 
     return (
         <Fragment>
-            <div className="bg-white h-full py-5 px-6 w-full rounded-lg">
+            <div className="bg-white h-screen py-5 px-6 w-full rounded-lg">
                 <Outlet/>
             </div>
         </Fragment>
