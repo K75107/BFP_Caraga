1b6cfcbb428a656e4540553ab61200e00c311b65
K75107 <divinagracia.kent123@gmail.com>
2024-12-08 00:49:43 +0800

    fix backend deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bfp-caraga-1.onrender.com', 
+  baseURL: 'https://bfp-caraga-2.onrender.com', 
 });
 
 export default api;
